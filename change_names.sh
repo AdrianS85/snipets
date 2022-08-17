bdd9e030c7b22d10701858ecd0a76f7cf1bfc74f
AdrianS85 <adrianstankiewicz85@gmail.com>
2022-08-17 15:08:14 +0100

    Update change_names.sh

--- change_names.sh
+++ change_names.sh
@@ -20,4 +20,4 @@ ls ${selector} | tee original_names.temp &&
 cat original_names.temp | sed 's/"$from"/"$to"/' | tee changed_names.temp
 paste original_names.temp changed_names.temp | tee both_names.temp
 parallel --colsep '\t' "mv {1} {2}" :::: both_names.temp
-rm original_names.temp changed_names.temp both_names.temp
+rm original_names.temp changed_names.temp
