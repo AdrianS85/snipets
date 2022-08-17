180860dbcf48cffb5e2d0cba69f5920c017369a7
AdrianS85 <adrianstankiewicz85@gmail.com>
2022-08-17 15:06:00 +0100

    Create change_names.sh

---
+++ change_names.sh
@@ -0,0 +1,22 @@
+# -s : bash-compatible expression for selecting files to change name of # SRR*
+# -f : change string from this (regex)
+# -t : to this
+# -d : directory in which files are present. If left alone - current folder # not implemented
+# -o : output directory # not implemented
+# bash make_short_files.sh -b RNA_S5227Nr -f fq -g true -n 100000
+
+
+
+while getopts b:f:gd:o:n: flag
+do
+	case "${flag}" in
+		s) selector=$OPTARG;;
+		f) from=$OPTARG;;
+		t) to=true;;
+	esac
+done
+
+ls ${selector} | tee original_names.temp &&
+cat original_names.temp | sed 's/"$from"/"$to"/' | tee changed_names.temp
+paste original_names.temp changed_names.temp | tee both_names.temp
+parallel --colsep '\t' "mv {1} {2}" :::: both_names.temp
